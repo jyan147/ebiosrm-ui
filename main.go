67c8539cefdf8b14c737bda357b3e21af223a51d
jyan147 <yoldas.guzelyildiz@groupe-esigelec.org>
2021-11-22 01:23:17 +0100

    news

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//user table
+
+// déclaration de la fonction affichage()
+func user(username string, password string) {
+	fmt.Println("#################################")
+	fmt.Println("\tBonjour" + username)
+	fmt.Println("#################################")
+}
+
+func getUserInput() string {
+
+	var (
+		EntryOk  = false // variable qui permet de vérifier si l'utilisateur a rentré la valeur qu'on attend de lui
+		username string
+		err      error
+		scanner  = bufio.NewScanner(os.Stdin)
+	)
+
+	for EntryOk == false {
+		fmt.Println("type username: ")
+		scanner.Scan()
+		username = scanner.Text()
+
+		if err != nil {
+			fmt.Println("Only strings !")
+		} else {
+			EntryOk = true
+			fmt.Println("Succes")
+		}
+	}
+
+	return username
+
+}
 
 func main() {
-	fmt.Println("Hello, this is my firts function")
+
+	getUserInput()
+
+	fmt.println("Your username is : ")
+
+	os.Exit(0)
+
 }
